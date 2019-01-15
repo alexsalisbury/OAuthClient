c77f5ac377ddddba8805fa34f1f4311593c26d36
Alex Salisbury <alex@alexsalisbury.com>
2019-01-15 14:52:50 -0500

    Add non-generic HTTP result class

---
+++ Home.OAuthClients.Tests/HttpResult.Tests.cs
@@ -0,0 +1,21 @@
+﻿namespace Home.OAuthClients.Tests
+{
+    using System.Net;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Home.OAuthClients.Models;
+
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class HttpResult_Tests
+    {
+        [TestMethod]
+        public void InstantiateResult()
+        {
+            string content = "test_content";
+            var httpResult = new HTTPResult(HttpStatusCode.Accepted, content, true);
+            Assert.IsNotNull(httpResult);
+            Assert.IsTrue(httpResult.Success);
+            Assert.AreEqual(content, httpResult.Content);
+            Assert.AreEqual(HttpStatusCode.Accepted, httpResult.HttpResponseCode);
+        }
+    }
+}
---
+++ Home.OAuthClients/Models/HTTPResult.cs
@@ -0,0 +1,18 @@
+﻿namespace Home.OAuthClients.Models
+{
+    using System.Net;
+
+    public class HTTPResult
+    {
+        public HttpStatusCode HttpResponseCode { get; private set; }
+        public string Content { get; private set; }
+        public bool Success { get; private set; }
+
+        public HTTPResult(HttpStatusCode responsecode, string content, bool success)
+        {
+            this.HttpResponseCode = responsecode;
+            this.Content = content;
+            this.Success = success;
+        }
+    }
+}
