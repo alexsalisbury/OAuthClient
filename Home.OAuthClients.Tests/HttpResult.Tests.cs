c23d4c2f50d1ef8c24623415235cd7229d444c12
Alex Salisbury <alex@alexsalisbury.com>
2019-01-15 15:46:53 -0500

    Implement Generic HttpResult<T> class. Bug 206 is tracking JSON serialization issue in test.

--- Home.OAuthClients.Tests/HttpResult.Tests.cs
+++ Home.OAuthClients.Tests/HttpResult.Tests.cs
@@ -77,4 +77,29 @@
             Assert.IsFalse(httpResult.Equals(httpResult2));
         }
     }
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class HttpResult_T_Tests
+    {
+        [TestMethod]
+        public void InstantiateResult()
+        {
+            string content = "test_content";
+            var contentObject = new HTTPResult(HttpStatusCode.Accepted, content, true);
+            var serializedResult = JsonConvert.SerializeObject(contentObject);
+
+            var httpResult = new HTTPResult<HTTPResult>(HttpStatusCode.Accepted, serializedResult, true);
+
+
+            Assert.IsNotNull(httpResult);
+            Assert.IsTrue(httpResult.Success);
+            Assert.IsTrue(httpResult.ParseSuccess);
+            Assert.AreEqual(serializedResult, httpResult.Content);
+            Assert.AreEqual(HttpStatusCode.Accepted, httpResult.HttpResponseCode);
+            Assert.IsNotNull(httpResult.Result);
+            //Assert.IsTrue(contentObject.Equals(httpResult.Result));
+            Assert.AreEqual(content, httpResult.Result.Content);
+            Assert.AreEqual(true, httpResult.Result.Success);
+            //Assert.AreEqual(HttpStatusCode.Accepted, httpResult.Result.HttpResponseCode);
+        }
+    }
 }
--- Home.OAuthClients/Models/HTTPResult.cs
+++ Home.OAuthClients/Models/HTTPResult.cs
@@ -1,8 +1,8 @@
 ﻿namespace Home.OAuthClients.Models
 {
-    using Newtonsoft.Json;
-    using System;
     using System.Net;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
 
     public class HTTPResult : System.IEquatable<HTTPResult>
     {
@@ -25,6 +25,35 @@
             }
 
             return this.Content == other.Content && this.HttpResponseCode == other.HttpResponseCode && this.Success == other.Success;
+        }
+    }
+
+    public class HTTPResult<T>
+    {
+        public HttpStatusCode HttpResponseCode { get; private set; }
+        public string Content { get; private set; }
+        public bool Success { get; private set; }
+        public bool ParseSuccess { get; private set; }
+        public T Result { get; private set; }
+
+        public HTTPResult(HttpStatusCode responsecode, string content, bool success)
+        {
+            this.HttpResponseCode = responsecode;
+            this.Content = content;
+            this.Success = success;
+            this.ParseSuccess = false;
+            try
+            {
+                if (success)
+                {
+                    this.Result = JsonConvert.DeserializeObject<T>(content);
+                    this.ParseSuccess = true;
+                }
+            }
+            catch
+            {
+
+            }
         }
     }
 }
