52c9a25a87edf4c756eeec8bcdd16255b5a3fcc3
Alex Salisbury <alex@alexsalisbury.com>
2019-01-17 17:38:19 -0500

    String extension for b64 encode

---
+++ Home.OAuthClients/Extensions/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Home.OAuthClients.Extensions
+{
+    internal static class StringExtensions
+    {
+        /// <summary>
+        /// Encodes a string to Base64.
+        /// </summary>
+        /// <param name="plainText">The string to encode.</param>
+        /// <returns>The base 64 encoded string.</returns>
+        public static string Base64Encode(this string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+    }
+}
