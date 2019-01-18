6ec9ad3a3e1b23867085c87a00e34c98e27bbbc6
Alex Salisbury <alex@alexsalisbury.com>
2019-01-18 08:01:23 -0500

    Add Token Object

---
+++ Home.OAuthClients.Tests/Models/Token.Tests.cs
@@ -0,0 +1,63 @@
+﻿namespace Home.OAuthClients.Tests.Models
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Home.OAuthClients.Models;
+
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class Token_Tests
+    {
+        [TestMethod]
+        public void TokenInstatiate()
+        {
+            var t = new Token();
+            Assert.IsTrue(DateTime.UtcNow > t.TokenGenerated);
+            Assert.IsTrue(DateTime.UtcNow - t.TokenGenerated < TimeSpan.FromSeconds(1));
+        }
+
+        [TestMethod]
+        public void TokenExpires()
+        {
+            var t = new Token()
+            {
+                ExpiresIn = 1,
+                RefreshExpiresIn = 3
+            };
+
+            Assert.IsFalse(t.IsExpired);
+            Assert.IsFalse(t.IsRefreshExpired);
+
+            //TODO: Should this get locked down? I prefer this to doing Thread.Sleep in the meantime.
+            t.ExpiresIn -= 2;
+            t.RefreshExpiresIn -= 2;
+
+            Assert.IsTrue(t.IsExpired);
+            Assert.IsFalse(t.IsRefreshExpired);
+
+            t.ExpiresIn -= 2;
+            t.RefreshExpiresIn -= 2;
+
+            Assert.IsTrue(t.IsExpired);
+            Assert.IsTrue(t.IsRefreshExpired);
+        }
+
+        [TestMethod]
+        public void TokenHeaders()
+        {
+            var expected = "Basic TestValue";
+            var t = new Token()
+            {
+                TokenType = "Basic",
+                AccessToken = "TestValue"
+            };
+
+            Assert.AreEqual(expected, t.HeaderString);
+
+            var authHeader = t.AuthHeader;
+
+            Assert.IsNotNull(authHeader);
+            Assert.AreEqual("Basic", authHeader.Scheme);
+            Assert.AreEqual("TestValue", authHeader.Parameter);
+        }
+    }
+}
---
+++ Home.OAuthClients/Models/Token.cs
@@ -0,0 +1,83 @@
+﻿namespace Home.OAuthClients.Models
+{
+    using System;
+    using System.Net.Http.Headers;
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// Represents a basic token 
+    /// </summary>
+    public class Token
+    {
+        /// <summary>
+        /// Generates a basic token, setting the token generated timestamp
+        /// </summary>
+        public Token()
+        {
+            this.TokenGenerated = DateTime.UtcNow;
+        }
+
+        /// <summary>
+        /// The Access Token for the service
+        /// </summary>
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+
+        /// <summary>
+        /// The refresh token to present when the token expires/
+        /// </summary>
+        [JsonProperty("refresh_token")]
+        public string RefreshToken { get; set; }
+
+        /// <summary>
+        /// The type of token.
+        /// TODO: List out types explicitly
+        /// </summary>
+        [JsonProperty("token_type")]
+        public string TokenType { get; set; }
+
+        /// <summary>
+        /// How long the token is valid for
+        /// </summary>
+        [JsonProperty("expires_in")]
+        public int ExpiresIn { get; set; }
+
+        /// <summary>
+        /// How long the refresh token is valid for
+        /// </summary>
+        [JsonProperty("refresh_token_expires_in")]
+        public int RefreshExpiresIn { get; set; }
+
+        /// <summary>
+        /// The time we generated this object
+        /// </summary>
+        [JsonProperty("token_generated")]
+        public DateTime TokenGenerated { get; private set; }
+
+        /// <summary>
+        /// Whether the token is expired
+        /// Note that due to lag, the server might not agree.
+        /// </summary>
+        [JsonIgnore]
+        public bool IsExpired => DateTime.UtcNow > this.TokenGenerated.AddSeconds(this.ExpiresIn);
+
+        /// <summary>
+        /// Whether the token is expired
+        /// Note that due to lag, the server might not agree.
+        /// </summary>
+        [JsonIgnore]
+        public bool IsRefreshExpired => DateTime.UtcNow > this.TokenGenerated.AddSeconds(this.RefreshExpiresIn);
+
+        /// <summary>
+        /// Header string for use
+        /// </summary>
+        [JsonIgnore]
+        public string HeaderString =>  $"{this.TokenType} {this.AccessToken}";
+
+        /// <summary>
+        /// Auth Header for use
+        /// </summary>
+        [JsonIgnore]
+        public AuthenticationHeaderValue AuthHeader => new AuthenticationHeaderValue(TokenType, AccessToken);
+    }
+}
