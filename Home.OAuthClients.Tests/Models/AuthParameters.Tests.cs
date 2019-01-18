f1054d6c0f9620c97b9433339ba31f6182f3aa7d
Alex Salisbury <alex@alexsalisbury.com>
2019-01-18 08:01:00 -0500

    Add Auth Parameters object

---
+++ Home.OAuthClients.Tests/Models/AuthParameters.Tests.cs
@@ -0,0 +1,25 @@
+﻿namespace Home.OAuthClients.Tests.Models
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Home.OAuthClients.Models;
+
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class AuthParameters_Tests
+    {
+        [TestMethod]
+        public void AuthParams_ClientString()
+        {
+            var result = $"client_id=Test&response_type=code&redirect_uri=localhost";
+            var ap = new AuthParameters()
+            {
+                ClientId = "Test",
+                ClientSecret = "Secret",
+                RedirectUri = "localhost"
+            };
+
+            var url = ap.GetQueryString();
+
+            Assert.IsFalse(string.IsNullOrWhiteSpace(url));
+        }
+    }
+}
---
+++ Home.OAuthClients/Models/AuthParameters.cs
@@ -0,0 +1,40 @@
+﻿namespace Home.OAuthClients.Models
+{
+    /// <summary>
+    /// The <see cref="AuthParameters"/>.
+    /// </summary>
+    public class AuthParameters
+    {
+
+        /// <summary>
+        /// Gets or sets the authentication client id.
+        /// </summary>
+        public string ClientId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the authentication client secret.
+        /// Note that this is only used for the AuthorizationCode flow.
+        /// </summary>
+        public string ClientSecret { get; set; }
+
+        /// <summary>
+        /// Gets or sets the authentication redirect uri.
+        /// </summary>
+        public string RedirectUri { get; set; }
+
+        /// <summary>
+        /// Generates a query string used for OAuth2 code requests
+        /// </summary>
+        /// <returns>url query string to be appended to base url</returns>
+        public virtual string GetQueryString()
+        {
+            //TODO: Should validate that RedirectUri is a Valid Uri?
+            var queryString =
+                   $"client_id={this.ClientId}" +
+                   $"&response_type=code" +
+                   $"&redirect_uri={this.RedirectUri}";
+
+            return queryString;
+        }
+    }
+}
