11f4cf9d9c5eafe98ca85a2c6436b68e3fa6bc3c
Alex Salisbury <alex@alexsalisbury.com>
2019-01-21 08:18:20 -0500

    Begin work on TodoistClient

---
+++ Home.TodoistClient.Tests/Converters/BoolConverter.Tests.cs
@@ -0,0 +1,51 @@
+﻿namespace Home.TodoistClient.Tests.Converters
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Home.Todoist.Converters;
+    using Newtonsoft.Json;
+
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class BoolConverter_Tests
+    {
+        public class MicroObject
+        {
+            [JsonProperty("t")]
+            [JsonConverter(typeof(BoolConverter))]
+            public bool T { get; set; }
+
+            [JsonProperty("f")]
+            [JsonConverter(typeof(BoolConverter))]
+            public bool F { get; set; }
+        }
+
+        [TestMethod]
+        public void CanConvertABool()
+        {
+            BoolConverter bc = new BoolConverter();
+
+            Assert.IsTrue(bc.CanConvert(typeof(bool)));
+            Assert.IsFalse(bc.CanConvert(bc.GetType()));
+            Assert.IsFalse(bc.CanConvert(string.Empty.GetType()));
+            Assert.IsFalse(bc.CanConvert(string.Empty.Length.GetType()));
+        }
+
+        [TestMethod]
+        public void ReadWriteSmoke()
+        {
+            string example = "{\"t\":1,\"f\":0}";
+            var item = JsonConvert.DeserializeObject<MicroObject>(example);
+
+            Assert.IsNotNull(item);
+            Assert.IsTrue(item.GetType() == typeof(MicroObject));
+            Assert.IsTrue(item.T);
+            Assert.IsFalse(item.F);
+
+            var ser = JsonConvert.SerializeObject(item);
+            Assert.AreEqual(example, ser);
+
+        }
+    }
+}
---
+++ Home.TodoistClient/Converters/BoolConverter.cs
@@ -0,0 +1,26 @@
+﻿namespace Home.Todoist.Converters
+{
+    using System;
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// https://stackoverflow.com/questions/14427596/convert-an-int-to-bool-with-json-net
+    /// </summary>
+    public class BoolConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(bool);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            return reader.Value.ToString() == "1";
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteValue(((bool)value) ? 1 : 0);
+        }
+    }
+}
