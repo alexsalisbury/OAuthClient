63dff79313796f7145e1335705336a0d2c2ef275
Alex Salisbury <alex@alexsalisbury.com>
2019-01-20 19:26:45 -0500

    Initial version of UWP library.

---
+++ Home.OAuthClients.UWP/Services/AuthService.cs
@@ -0,0 +1,40 @@
+﻿namespace Home.OAuthClients.UWP
+{
+    using System;
+    using System.Threading.Tasks;
+    using Windows.Security.Authentication.Web;
+    using Newtonsoft.Json;
+    using Home.OAuthClients.Models;
+
+    public interface IAuthService
+    {
+        Task<T> GetTokenAsync<T>(Uri startUri, WebAuthenticationOptions options) where T : Token;
+        Task<string> GetTokenAsync(Uri startUri, WebAuthenticationOptions options);
+    }
+
+    public class AuthService: IAuthService
+    {
+        public virtual async Task<T> GetTokenAsync<T>(Uri startUri, WebAuthenticationOptions options = WebAuthenticationOptions.None) where T : Token
+        {
+            string token = await GetTokenAsync(startUri);
+
+            if (string.IsNullOrEmpty(token))
+            { 
+                return null;
+            }
+
+            return JsonConvert.DeserializeObject<T>(token);
+        }
+
+        public virtual async Task<string> GetTokenAsync(Uri startUri, WebAuthenticationOptions options = WebAuthenticationOptions.None)
+        {
+            var result = await WebAuthenticationBroker.AuthenticateAsync(options, startUri);
+            if (result.ResponseStatus == WebAuthenticationStatus.Success)
+            {
+                return result.ResponseData.ToString();
+            }
+
+            return null;
+        }
+    }
+}
