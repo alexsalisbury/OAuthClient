975b9c4f7f35d833c0af1dd82f4084ee38ec1268
Alex Salisbury <alex@alexsalisbury.com>
2019-01-21 08:46:31 -0500

    Add Code Coverage to Todoist Tests

--- codecov.ps1
+++ codecov.ps1
@@ -9,3 +9,7 @@ $fName = ".\Home.OAuthClients.Tests\coverage.opencover.xml";
 $arg1 = "-f ""$fName""";
 $arg2 = "-t $token";
 & $cmd $arg1 $arg2
+
+$fName = ".\Home.TodoistClient.Tests\coverage.opencover.xml";
+$arg1 = "-f ""$fName""";
+& $cmd $arg1 $arg2
